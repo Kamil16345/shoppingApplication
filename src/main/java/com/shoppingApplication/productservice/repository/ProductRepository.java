3b776c253f84495ccca4adb3b530620679286ad8
Kamil <KamilArkit@interia.pl>
2023-09-10 20:54:07 +0200

    Refactored project structure - in progress.

--- src/main/java/com/shoppingApplication/productservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shoppingApplication.productservice.repository;
-
-import com.shoppingApplication.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
