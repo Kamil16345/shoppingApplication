41ab6ad2921fce57a76185d567da4b7ca03d7698
Kamil <KamilArkit@interia.pl>
2023-09-11 23:30:12 +0200

    Update of structure and added order service.

---
+++ product-service/src/main/java/com/shoppingApplication/productservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.shoppingApplication.productservice.repository;
+
+import com.shoppingApplication.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
