2b41c6361aa52c73c70189148ead01c9ffcaa0b2
Kamil <KamilArkit@interia.pl>
2023-09-13 00:31:30 +0200

    Created functionalities for order-service like placing and order to DB and mapping it to DTO. Some errors occurs caused by hibernate configuration so far.

---
+++ order-service/src/main/java/com/shoppingApplication/orderservice/controller/OrderController.java
@@ -0,0 +1,20 @@
+package com.shoppingApplication.orderservice.controller;
+
+import com.shoppingApplication.orderservice.dto.OrderRequest;
+import com.shoppingApplication.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "Order has been created.";
+    }
+}
