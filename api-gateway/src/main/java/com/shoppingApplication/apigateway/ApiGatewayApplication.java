ffdb860494bedd6c6057d54c52c021e188d78f26
Kamil <KamilArkit@interia.pl>
2023-09-20 20:13:52 +0200

    Introduced API-gateway, for now - only introduced, it is not working.

---
+++ api-gateway/src/main/java/com/shoppingApplication/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,9 @@
+package com.shoppingApplication.apigateway;
+
+@SpringBootApplication
+
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringBootApplication.run(ApiGatewayApplication.class, args);
+    }
+}
