5144fa45969e22813aad05aea3cb2e67272a0f86
Kamil <KamilArkit@interia.pl>
2023-09-22 18:18:34 +0200

    Implemented api-gateway for product, order and discovery services

--- api-gateway/src/main/java/com/shoppingApplication/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/shoppingApplication/apigateway/ApiGatewayApplication.java
@@ -1,9 +1,12 @@
 package com.shoppingApplication.apigateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 
 public class ApiGatewayApplication {
     public static void main(String[] args) {
-        SpringBootApplication.run(ApiGatewayApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
