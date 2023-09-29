8c09404b13ccc5c5da4741b9fbf23a3ed5abb6ab
Kamil <KamilArkit@interia.pl>
2023-09-30 00:44:49 +0200

    Refactored test for posting an order.

--- order-service/src/main/java/com/shoppingApplication/orderservice/dto/OrderRequest.java
+++ order-service/src/main/java/com/shoppingApplication/orderservice/dto/OrderRequest.java
@@ -2,12 +2,14 @@ package com.shoppingApplication.orderservice.dto;
 
 import com.shoppingApplication.orderservice.model.OrderLineItems;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderRequest {
--- order-service/src/test/java/com/shoppingApplication/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/shoppingApplication/orderservice/OrderServiceApplicationTests.java
@@ -2,9 +2,9 @@ package com.shoppingApplication.orderservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shoppingApplication.orderservice.dto.OrderLineItemsDto;
+import com.shoppingApplication.orderservice.dto.OrderRequest;
+import com.shoppingApplication.orderservice.model.OrderLineItems;
 import com.shoppingApplication.orderservice.repository.OrderRepository;
-import com.shoppingApplication.orderservice.service.OrderService;
-import net.bytebuddy.utility.dispatcher.JavaDispatcher;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +22,10 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.containers.MySQLContainer;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -29,13 +33,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @AutoConfigureMockMvc
 class OrderServiceApplicationTests {
 	@Container
-	static MySQLContainer MY_SQL_CONTAINER = new MySQLContainer(DockerImageName.parse("mysql").withTag("5.7.34"))
-			.withDatabaseName("yourdatabase");
+	static MySQLContainer MY_SQL_CONTAINER = new MySQLContainer(DockerImageName.parse("mysql").withTag("5.7.34"));
 
 	@DynamicPropertySource
 	static void mysqlProperties(DynamicPropertyRegistry registry) {
 		MY_SQL_CONTAINER.start();
-
 		registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
 		registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
 		registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
@@ -52,11 +54,20 @@ class OrderServiceApplicationTests {
 	OrderLineItemsDto orderLineItemsDto;
 	@Test
 	public void shouldPlaceOrder() throws Exception {
+		OrderRequest orderLineItemsDto = getOrderRequest();
 		String productRequestString = objectMapper.writeValueAsString(orderLineItemsDto);
 		mockMvc.perform(MockMvcRequestBuilders.post("/api/order")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(productRequestString))
 				.andExpect(MockMvcResultMatchers.status().isCreated());
-		Assertions.assertTrue(orderRepository.findAll().size() == 1);
+	}
+	public OrderRequest getOrderRequest(){
+		OrderLineItemsDto orderLineItems = new OrderLineItemsDto("123-123", BigDecimal.valueOf(22), 3);
+//		List<OrderLineItemsDto> orderLineItemsDtoList = null;
+		List<OrderLineItemsDto> orderLineItemsDtoList = new ArrayList<>();
+		orderLineItemsDtoList.add(orderLineItems);
+		return OrderRequest.builder()
+				.orderLineItemsDtoList(orderLineItemsDtoList)
+				.build();
 	}
 }
